edc415cf735b8df4665c7bdd81becabd73e1d7d8
Ishan Sinha <forishansinha@outlook.com>
2020-01-11 18:46:37 -0500

    PathFollower classes for advanced auton movement, for some reason didn't push originally.

--- src/main/java/frc/robot/DriveData.java
+++ src/main/java/frc/robot/DriveData.java
@@ -7,8 +7,24 @@
 
 package frc.robot;
 
-/**
- * Add your docs here.
- */
 public class DriveData {
+	
+	private float Distance;
+	private float Angle;
+	
+	public float getDistance() {
+		return Distance;
+	}
+	
+	public void setDistance(float distance) {
+		Distance = distance;
+	}
+	
+	public float getAngle() {
+		return Angle;
+	}
+	
+	public void setAngle(float angle) {
+		Angle = angle;
+	}
 }
