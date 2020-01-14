9255bce07a19035a73745c680b84ed22a894bba6
Ishan Sinha <forishansinha@outlook.com>
2020-01-13 20:31:47 -0500

    Vision mostly there, just need to incorporate it into our turret class, when we get a physical turret to test. Also, when we get that we need to add a bit more to the class to setTheTurretHoodAngle after testing.

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,42 +1,63 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import java.lang.Math;
+
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
 
     private static float CameraHeight = 36;
-    private static float TargetHeight = 90;
+    private static float TargetHeight = 92;
     private static float CameraAngle = 30;
     private static float LIMELIGHT_Y_AXIS_PIXELS = 240;
+    private NetworkTable table = null;
+
+    public Vision () {
+      table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
 
     public VisionData getDistanceAndAngleOffTarget (VisionData data) {
 
-        double degrees;
-        double ty0;
-        double degreesTargetOffGround;
-        double distance;   
-        double angleOffTarget;
+      double degrees;
+      double ty0;
+      double degreesTargetOffGround;
+      double distance;   
+      double angleOffTarget;
 
-        ty0 = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty0").getDouble(0);
-        degrees = ty0 *(LIMELIGHT_Y_AXIS_PIXELS/2);
-        degreesTargetOffGround = CameraAngle - degrees;
-        distance = (TargetHeight - CameraHeight) / Math.tan(degreesTargetOffGround);
-        angleOffTarget = ty0 = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+      setVisionTrackingMode();
+      setLED(true);
+      ty0 = table.getEntry("ty0").getDouble(0);
+      degrees = ty0 *(LIMELIGHT_Y_AXIS_PIXELS/2);
+      degreesTargetOffGround = CameraAngle - degrees;
+      distance = (TargetHeight - CameraHeight) / Math.tan(degreesTargetOffGround);
+      angleOffTarget = ty0 = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
 
-        System.out.println("distance from target:" + distance);
-        System.out.println("angle off center target:" + angleOffTarget);
+      System.out.println("distance from target:" + distance);
+      System.out.println("angle off center target:" + angleOffTarget);
         
-        data.setAngleOffTarget((float)angleOffTarget);
-        data.setDistanceAwayFromTarget((float)distance);
+      data.setAngleOffTarget((float)angleOffTarget);
+      data.setDistanceAwayFromTarget((float)distance);
 
-        return data;
+       return data;
     }
+
+    public void setLED(boolean turnOn) {
+		table.getEntry("ledMode").forceSetNumber(turnOn ? 3 : 1); // 3 - on, 1 = off, 2 - blink
+    }
+    
+	public void flashLED(){
+		table.getEntry("ledMode").forceSetNumber(2);
+    }
+    
+    public void setDriverMode() {
+		setLED(false);
+		table.getEntry("camMode").forceSetNumber(1);
+	}
+
+	private void setVisionTrackingMode() {
+		table.getEntry("camMode").forceSetNumber(0);
+    }
+
 }
+
