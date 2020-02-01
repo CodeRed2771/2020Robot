c0ddc89635a8a64d05341516531b90252af0d654
Dale Van Voorst <dale@dalevanvoorst.com>
2020-02-01 16:21:39 -0500

    Vision tweaks - added ability to choose pipelines

--- src/main/java/frc/robot/AutoAlign.java
+++ src/main/java/frc/robot/AutoAlign.java
@@ -4,66 +4,67 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 //The purpose of this class is to turn the robot until we are on target.
 
-public class AutoAlign extends AutoBaseClass{
+public class AutoAlign extends AutoBaseClass {
 
-private double angleOffset = 0;
+    private double angleOffset = 0;
 
-    public void start(){
+    public void start() {
         super.start();
         Vision.getInstance();
         Vision.setVisionTrackingMode();
+        Vision.setTargetForShooting();
     }
-    public void stop(){
+
+    public void stop() {
         super.stop();
     }
 
     @Override
     public void tick() {
-        if (isRunning()){
-        DriveAuto.tick();
-        SmartDashboard.putNumber("Auto Step", getCurrentStep());
-            switch (getCurrentStep()){
-                case 0:
-                    angleOffset = Vision.getDistanceAdjustedAngle();
-                    if (Vision.seesTarget() == true) {
-                        advanceStep();
-                    } else if (Vision.seesTarget() == false) {
-                        setStep(10);
-                    }
-                    break;           
-                case 1: //thought: one potential issue that could be happening is if the camera was picking up a target that wasn't actually the target... ~AR 
-                    DriveAuto.turnDegrees(angleOffset, .6);
-                    setTimerAndAdvanceStep(2000);
-                    break;
+        if (isRunning()) {
+            DriveAuto.tick();
+            SmartDashboard.putNumber("Auto Step", getCurrentStep());
+            switch (getCurrentStep()) {
+            case 0:
+                angleOffset = Vision.getDistanceAdjustedAngle();
+                if (Vision.seesTarget() == true) {
+                    advanceStep();
+                } else if (Vision.seesTarget() == false) {
+                    setStep(10);
+                }
+                break;
 
-                case 2:
-                    if (DriveAuto.hasArrived()){
-                        advanceStep();
-                    }
-                    break;
-                case 3:
-                    angleOffset = Vision.getDistanceAdjustedAngle();
-                    SmartDashboard.putNumber("Angle Offset", angleOffset);
-                    SmartDashboard.putBoolean("Sees Target", Vision.seesTarget());
-                    if (Vision.onTarget()){
-                        System.out.println("On Target!");
-                        stop();
-                    }
-                    else {
-                        setStep(0);
-                    }
-                    break;
-                case 10:
-                    turnDegrees(60, 1);
-                    if (Vision.seesTarget() == true) {
-                        setStep(0);
-                    }
-                    break;
+            case 1: // thought: one potential issue that could be happening is if the camera was
+                    // picking up a target that wasn't actually the target... ~AR
+                DriveAuto.turnDegrees(angleOffset, .6);
+                setTimerAndAdvanceStep(2000);
+                break;
+
+            case 2:
+                if (DriveAuto.hasArrived()) {
+                    advanceStep();
+                }
+                break;
+            case 3:
+                angleOffset = Vision.getDistanceAdjustedAngle();
+                SmartDashboard.putNumber("Angle Offset", angleOffset);
+                SmartDashboard.putBoolean("Sees Target", Vision.seesTarget());
+                if (Vision.onTarget()) {
+                    System.out.println("On Target!");
+                    stop();
+                } else {
+                    setStep(0);
+                }
+                break;
+            case 10:
+                turnDegrees(60, 1);
+                if (Vision.seesTarget() == true) {
+                    setStep(0);
+                }
+                break;
             }
         }
 
-
     }
-   
-    
+
 }
--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -6,82 +6,90 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
 
-  private static double CameraHeight = 9; // NEED TO BE MORE ACCURATE
-  private static double TargetHeight = 92; // NEED TO BE MORE ACCURATE
-  public static double CameraAngle = 18;  // NEED TO BE MORE ACCURATE
-  private static double LIMELIGHT_Y_AXIS_FOV = 45.7;
-  private static NetworkTable table = null;
-  private static double angleOffTarget = 0;
-  private static double ty = 0;
-  private static double degreesTargetOffGround = 0;
-  private static double distance = 0;
-    
-  public static Vision instance;
+    private static double CameraHeight = 9; // NEED TO BE MORE ACCURATE
+    private static double TargetHeight = 92; // NEED TO BE MORE ACCURATE
+    public static double CameraAngle = 18; // NEED TO BE MORE ACCURATE
+    private static double LIMELIGHT_Y_AXIS_FOV = 45.7;
+    private static NetworkTable table = null;
+    private static double angleOffTarget = 0;
+    private static double ty = 0;
+    private static double degreesTargetOffGround = 0;
+    private static double distance = 0;
 
-  public static Vision getInstance(){
-    if (instance == null)
-        instance = new Vision();
-      return instance;
-  }
-  
-  public Vision () {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-  }
+    public static Vision instance;
 
-  public static double getAngleOffset(){
-    return table.getEntry("tx").getDouble(0);
-  }
+    public static Vision getInstance() {
+        if (instance == null)
+            instance = new Vision();
+        return instance;
+    }
 
-  public static double getDistanceAdjustedAngle() {
+    public Vision() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
 
-    return (getAngleOffset()+(-(getDistanceFromTarget()-180)/360));
+    public static double getAngleOffset() {
+        return table.getEntry("tx").getDouble(0);
+    }
 
-    // if (getDistanceFromTarget() < 180) {  // 15 feet
-    //     return getAngleOffset() + ((180 - getDistanceFromTarget()) * .01);
-    // } else {
-    //     return getAngleOffset() - ((getDistanceFromTarget() - 180) * .01);
-    // }
+    public static double getDistanceAdjustedAngle() {
 
-  }
+        return (getAngleOffset() + (-(getDistanceFromTarget() - 180) / 360));
 
-  public static boolean seesTarget(){
-    return table.getEntry("tv").getDouble(0) >0;
-  }
-  
-  public static boolean onTarget() {
-    return (seesTarget() && (Math.abs(getAngleOffset()) <= 1));
-  }
+        // if (getDistanceFromTarget() < 180) { // 15 feet
+        // return getAngleOffset() + ((180 - getDistanceFromTarget()) * .01);
+        // } else {
+        // return getAngleOffset() - ((getDistanceFromTarget() - 180) * .01);
+        // }
 
-  public static double getDistanceFromTarget () {
-    ty = table.getEntry("ty").getDouble(0);
-    degreesTargetOffGround = CameraAngle + ty;
-    distance = (TargetHeight - CameraHeight) / Math.tan(Math.toRadians(degreesTargetOffGround));
-    return distance;
-  }
+    }
 
-  public static void setLED (boolean turnOn) {
-		table.getEntry("ledMode").forceSetNumber(turnOn ? 3 : 1); // 3 - on, 1 = off, 2 - blink
-  }
-    
-	public static void flashLED (){
-		table.getEntry("ledMode").forceSetNumber(2);
-  }
-    
-  public static void setDriverMode () {
-		setLED(false);
-		table.getEntry("camMode").forceSetNumber(1);
-	}
+    public static boolean seesTarget() {
+        return table.getEntry("tv").getDouble(0) > 0;
+    }
 
-	public static void setVisionTrackingMode () {
-    setLED(true);
-		table.getEntry("camMode").forceSetNumber(0);
-  }
+    public static boolean onTarget() {
+        return (seesTarget() && (Math.abs(getAngleOffset()) <= 1));
+    }
 
-  public static void setVisionToActiveTrackingMode () {
-    table.getEntry("snapshot").forceSetNumber(1);
-  }
+    public static double getDistanceFromTarget() {
+        ty = table.getEntry("ty").getDouble(0);
+        degreesTargetOffGround = CameraAngle + ty;
+        distance = (TargetHeight - CameraHeight) / Math.tan(Math.toRadians(degreesTargetOffGround));
+        return distance;
+    }
 
-  public static void stopActiveVisionMode () {
-    table.getEntry("snapshot").forceSetNumber(0);
-  }
+    public static void setLED(boolean turnOn) {
+        table.getEntry("ledMode").forceSetNumber(turnOn ? 3 : 1); // 3 - on, 1 = off, 2 - blink
+    }
+
+    public static void flashLED() {
+        table.getEntry("ledMode").forceSetNumber(2);
+    }
+
+    public static void setDriverMode() {
+        setLED(false);
+        table.getEntry("camMode").forceSetNumber(1);
+    }
+
+    public static void setVisionTrackingMode() {
+        setLED(true);
+        table.getEntry("camMode").forceSetNumber(0);
+    }
+
+    public static void setVisionToActiveTrackingMode() {
+        table.getEntry("snapshot").forceSetNumber(1);
+    }
+
+    public static void stopActiveVisionMode() {
+        table.getEntry("snapshot").forceSetNumber(0);
+    }
+
+    public static void setTargetForShooting() {
+        table.getEntry("pipeline").forceSetNumber(0);
+    }
+
+    public static void setTargetForFuelCell() {
+        table.getEntry("pipeline").forceSetNumber(1);
+    }
 }
