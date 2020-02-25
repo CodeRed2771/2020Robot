cfd3804bba39feaad9e509c929831923d22fbdfb
Dale Van Voorst <dale@dalevanvoorst.com>
2020-02-24 22:22:24 -0500

    Updates to DriveFromLoadingStation

--- src/main/java/frc/robot/AutoDriveFromLoadingStation.java
+++ src/main/java/frc/robot/AutoDriveFromLoadingStation.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import javax.lang.model.util.ElementScanner6;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -38,6 +40,7 @@ public class AutoDriveFromLoadingStation extends AutoBaseClass {
                     break;
                 case 4:
                     driveInches(160, 35, 1); 
+                    setTimerAndAdvanceStep(3000);
                     break;
                 case 5:
                     if (driveCompleted()){
@@ -45,22 +48,30 @@ public class AutoDriveFromLoadingStation extends AutoBaseClass {
                     }
                     break;
                 case 6:
-                    DistanceSensor.getRange();
+                    advanceStep();
                     break;
                 case 7:
-                    if (DistanceSensor.getRange() >= 12){
-                        driveInches(12, 45, 1, false, true);
-                    } else if (DistanceSensor.getRange() <= 12){
-                        driveInches(120, 0, 1, false, true);
-                        setTimerAndAdvanceStep(2000);
-                    }
+                    if (DistanceSensor.getRange() > 12){
+                        driveInches(DistanceSensor.getRange() - 6, 90, 1, false, true);
+                        setTimerAndAdvanceStep(2000);  
+                    } else {
+                        setStep(getCurrentStep() + 2);
+                         } 
                     break;
-                case 8:
+                case 8: 
                     if (driveCompleted()){
                         advanceStep();
                     }
                     break;
                 case 9:
+                    driveInches(120, 0, 1, false, true);
+                    setTimerAndAdvanceStep(5000);
+                    break;
+                case 10:
+                    if (driveCompleted()){
+                        advanceStep();
+                    }
+                case 11:
                     stop();
                     break;
                 
